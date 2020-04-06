77a984e6e1b4ca2c5f9bc88636dd5bbb6db206a0
Janne Mattila <janne.mattila@hotmail.fi>
2020-04-06 15:38:33 +0300

    Add placeholder test project

---
+++ test/unit/MyChess.Tests/ChessBoardTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace MyChess.Tests
+{
+    public class ChessBoardTests
+    {
+        private readonly ChessBoard _board;
+
+        public ChessBoardTests()
+        {
+            _board = new ChessBoard();
+            _board.Initialize();
+        }
+
+        [Fact]
+        public void AvailableMoves_In_Start_Test()
+        {
+            // Arrange
+            var expected = 20;
+
+            // Act
+            var actual = _board.GetAllAvailableMoves().Length;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
