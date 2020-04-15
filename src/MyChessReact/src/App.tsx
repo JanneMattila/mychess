4e378423ab5707eb61e786b98f0097877899ee2f
Janne Mattila <janne.mattila@hotmail.fi>
2020-04-15 22:03:42 +0300

    Add router support

--- src/MyChessReact/src/App.tsx
+++ src/MyChessReact/src/App.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="My Chess" />
-        <br />My Chess is coming. Stay tuned.
-      </header>
-    </div>
+    <Router>
+      <div>
+        <div className="App">
+          <Link to="/" className="App-link">My Chess</Link>
+
+          <Switch>
+            <Route exact path="/">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="My Chess" />
+                <br />My Chess is coming. Stay tuned.
+              </header>
+            </Route>
+            <Route path="/settings">
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
