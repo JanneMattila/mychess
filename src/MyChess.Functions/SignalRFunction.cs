04bf90a0cde7e5cf2949409fe23f0dedbceac75c
Janne Mattila <janne.mattila@hotmail.fi>
2020-04-18 11:45:03 +0300

    Add SignalR Service function

---
+++ src/MyChess.Functions/SignalRFunction.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+
+namespace MyChess.Functions
+{
+    public class SignalRFunction
+    {
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo GetSignalRInfo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "game")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+    }
+}
