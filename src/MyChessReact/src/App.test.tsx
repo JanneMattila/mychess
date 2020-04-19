97bd7576afa8b05195c35aca11390d4a3f8a2b96
Janne Mattila <janne.mattila@hotmail.fi>
2020-04-19 11:17:56 +0300

    Fix test and clean up files

--- src/MyChessReact/src/App.test.tsx
+++ src/MyChessReact/src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./reducers";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+const store = createStore(rootReducer);
+
+test('renders My Chess is coming', () => {
+  const { getByText } = render(<Provider store={store}><App /></Provider >);
+  const linkElement = getByText(/My Chess is coming/i);
   expect(linkElement).toBeInTheDocument();
 });
