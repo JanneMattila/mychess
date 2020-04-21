ffbd0eaa87bff72de2f0ff880f1c8ed1173262ca
Janne Mattila <janne.mattila@hotmail.fi>
2020-04-21 18:03:07 +0300

    Add placeholder games api

---
+++ src/MyChess.Functions/GamesFunction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using MyChess.Functions.Interfaces;
+
+namespace MyChess.Functions
+{
+    public static class GamesFunction
+    {
+        [FunctionName("Games")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            using var scope = log.BeginScope("Games");
+            log.LogInformation("Games function processing request.");
+
+            var games = new List<Game>();
+            for (int i = 0; i < 5; i++)
+            {
+                games.Add(new Game()
+                {
+                    ID = Guid.NewGuid().ToString("B"),
+                    Name = $"Game of name {i + 1}",
+                    Opponent = $"User {i + 1}",
+                    Updated = DateTimeOffset.UtcNow.AddHours(-i)
+                });
+            }
+
+            return new OkObjectResult(games);
+        }
+    }
+}
---
+++ src/MyChess.Functions/Interfaces/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace MyChess.Functions.Interfaces
+{
+    public class Game
+    {
+        [JsonProperty(PropertyName = "id", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        public string ID { get; set; }
+
+        [JsonProperty(PropertyName = "name", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "opponent", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        public string Opponent { get; set; }
+
+        [JsonProperty(PropertyName = "updated", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        public DateTimeOffset Updated { get; set; }
+    }
+}
