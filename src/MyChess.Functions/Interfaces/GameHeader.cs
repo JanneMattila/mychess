ea0b19124427b6a459e8673f2c02a82d6f357e64
Janne Mattila <janne.mattila@hotmail.fi>
2020-04-27 17:29:59 +0300

    Fix nullable warnings in Azure func project

--- src/MyChess.Functions/Interfaces/GameHeader.cs
+++ src/MyChess.Functions/Interfaces/GameHeader.cs
@@ -6,16 +6,16 @@ namespace MyChess.Functions.Interfaces
     public class GameHeader
     {
         [JsonProperty(PropertyName = "id", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
-        public string ID { get; set; }
+        public string ID { get; set; } = string.Empty;
 
         [JsonProperty(PropertyName = "name", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
         [JsonProperty(PropertyName = "opponent", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
-        public string Opponent { get; set; }
+        public string Opponent { get; set; } = string.Empty;
 
         [JsonProperty(PropertyName = "comment", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
-        public string Comment { get; set; }
+        public string Comment { get; set; } = string.Empty;
 
         [JsonProperty(PropertyName = "updated", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
         public DateTimeOffset Updated { get; set; }
