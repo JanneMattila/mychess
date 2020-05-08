861992bd92c9039619bdf1e3034ec17b602771f5
Janne Mattila <janne.mattila@hotmail.fi>
2020-05-08 16:36:13 +0300

    Add friends  page

---
+++ src/MyChessReact/src/pages/Friends.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import "./Friends.css";
+
+export function Friends() {
+
+    useEffect(() => {
+    });
+
+    const closeModal = () => {
+    }
+
+    return (
+        <div>
+            <header className="Friends-header">
+                <h4>Friends</h4>
+            </header>
+        </div>
+    );
+}
