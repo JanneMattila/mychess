af11dc40d98f78ea46528e596cf49645fe324e0d
Janne Mattila <janne.mattila@hotmail.fi>
2020-05-09 21:31:00 +0300

    Add functions test project

---
+++ test/unit/MyChess.Functions.Tests/GamesFunctionTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+using MyChess.Functions.Tests.Stubs;
+using Xunit;
+
+namespace MyChess.Functions.Tests
+{
+    public class GamesFunctionTests
+    {
+        private readonly GamesFunction _gamesFunction;
+        private readonly SecurityValidatorStub _securityValidatorStub;
+
+        public GamesFunctionTests()
+        {
+            _securityValidatorStub = new SecurityValidatorStub();
+            _gamesFunction = new GamesFunction(_securityValidatorStub);
+        }
+
+        [Fact]
+        public async Task No_Claims_Test()
+        {
+            // Arrange
+            var expected = typeof(UnauthorizedResult);
+
+            // Act
+            var actual = await _gamesFunction.Run(null, NullLogger.Instance);
+
+            // Assert
+            Assert.IsType(expected, actual);
+        }
+    }
+}
---
+++ test/unit/MyChess.Functions.Tests/Stubs/SecurityValidatorStub.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace MyChess.Functions.Tests.Stubs
+{
+    public class SecurityValidatorStub : ISecurityValidator
+    {
+        public async Task<ClaimsPrincipal> GetClaimsPrincipalAsync(HttpRequest req, ILogger log)
+        {
+            return await Task.FromResult<ClaimsPrincipal>(null);
+        }
+    }
+}
