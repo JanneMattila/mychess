2ba1099c6edb7c7d07af66a1c5439acfcbe866aa
Janne Mattila <janne.mattila@hotmail.fi>
2020-05-09 21:48:24 +0300

    Add authenticated user model

---
+++ src/MyChess/Models/AuthenticatedUserModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MyChess.Interfaces
+{
+    public class AuthenticatedUserModel
+    {
+        public string UserIdentifier { get; set; } = string.Empty;
+
+        public string ProviderIdentifier { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string PreferredUsername { get; set; } = string.Empty;
+    }
+}
