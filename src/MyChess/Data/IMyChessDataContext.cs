bf9450c84541f859b69e9e0d95e6ff8a2aa506e0
Janne Mattila <janne.mattila@hotmail.fi>
2020-05-09 21:48:15 +0300

    Fix nullability warning

--- src/MyChess/Data/IMyChessDataContext.cs
+++ src/MyChess/Data/IMyChessDataContext.cs
@@ -7,7 +7,7 @@ namespace MyChess.Data
     public interface IMyChessDataContext
     {
         IAsyncEnumerable<T> GetAllAsync<T>(string tableName, string partitionKey) where T : TableEntity, new();
-        Task<T> GetAsync<T>(string tableName, string partitionKey, string rowKey) where T : TableEntity;
+        Task<T?> GetAsync<T>(string tableName, string partitionKey, string rowKey) where T : TableEntity;
         void Initialize();
         Task<TableResult> UpsertAsync<T>(string tableName, T entity) where T : TableEntity;
     }
