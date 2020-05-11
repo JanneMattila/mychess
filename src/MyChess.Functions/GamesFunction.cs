fb9af95544f881663d699f203f9790d119460e24
Janne Mattila <janne.mattila@hotmail.fi>
2020-05-11 17:44:16 +0300

    Return real game data from api

--- src/MyChess.Functions/GamesFunction.cs
+++ src/MyChess.Functions/GamesFunction.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using MyChess.Handlers;
-using MyChess.Interfaces;
 
 namespace MyChess.Functions
 {
@@ -45,28 +42,7 @@ namespace MyChess.Functions
                 return new UnauthorizedResult();
             }
 
-            var userGames = await _gamesHandler.GetGamesAsync(principal.ToAuthenticatedUser());
-
-            var games = new List<MyChessGame>();
-            for (int i = 0; i < 5; i++)
-            {
-                var game = new MyChessGame()
-                {
-                    ID = Guid.NewGuid().ToString("D"),
-                    Name = $"Game of name {i + 1}",
-                    Updated = DateTimeOffset.UtcNow.AddHours(-i)
-                };
-                game.Players.Black.Name = $"User {i + 1}";
-                game.Moves.Add(new MyChessGameMove()
-                {
-                    Comment = "👍 Lorem ipsum dolor sit amet ❤ 😊, consectetur adipiscing elit. Ut sed mollis neque. Maecenas molestie nibh id elit gravida, quis placerat magna tempor. Morbi posuere orci sapien, eget dictum ligula tempor ut. Vivamus nec massa dolor. Sed fermentum ex non nunc dapibus blandit. Vivamus sollicitudin, libero rhoncus faucibus ullamcorper, velit dui finibus neque, sed placerat sapien urna id nunc. Aliquam ac consectetur elit.",
-                    Move = "A2A4",
-                    Start = DateTimeOffset.UtcNow.AddHours(-i),
-                    End = DateTimeOffset.UtcNow,
-                });
-                games.Add(game);
-            }
-
+            var games = await _gamesHandler.GetGamesAsync(principal.ToAuthenticatedUser());
             return new OkObjectResult(games);
         }
     }
