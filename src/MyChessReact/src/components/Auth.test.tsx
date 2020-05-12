8ac013d780b9ef488c97682748a80e7d43778891
Janne Mattila <janne.mattila@hotmail.fi>
2020-05-12 13:49:23 +0300

    Fix auth router issue in test

--- src/MyChessReact/src/components/Auth.test.tsx
+++ src/MyChessReact/src/components/Auth.test.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import { render } from "@testing-library/react";
 import { Auth } from "./Auth";
 import { Provider } from "react-redux";
@@ -8,7 +9,7 @@ import rootReducer from "../reducers";
 const store = createStore(rootReducer);
 
 test("renders sign in", () => {
-  const { getByText } = render(<Provider store={store}><Auth clientId="a" applicationIdURI="b" /></Provider>);
+  const { getByText } = render(<Provider store={store}><Router><Auth clientId="a" applicationIdURI="b" /></Router></Provider>);
   const signInElement = getByText(/Sign In/i);
   expect(signInElement).toBeInTheDocument();
 });
