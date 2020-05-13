aaece7559df176516edf3324a576bd08c1c8686a
Janne Mattila <janne.mattila@hotmail.fi>
2020-05-13 14:39:01 +0300

    Add unit tests for games function

--- test/unit/MyChess.Functions.Tests/GamesFunctionTests.cs
+++ test/unit/MyChess.Functions.Tests/GamesFunctionTests.cs
@@ -1,8 +1,11 @@
-﻿using System.Security.Claims;
+﻿using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging.Abstractions;
+using MyChess.Functions.Tests.Helpers;
 using MyChess.Functions.Tests.Stubs;
+using MyChess.Interfaces;
 using Xunit;
 
 namespace MyChess.Functions.Tests
@@ -45,6 +48,60 @@ namespace MyChess.Functions.Tests
 
             // Assert
             Assert.IsType(expected, actual);
+        }
+
+        [Fact]
+        public async Task Fetch_Games_Test()
+        {
+            // Arrange
+            var expected = typeof(OkObjectResult);
+            var expectedGames = 2;
+            
+            _gamesHandlerStub.Games.Add(new MyChessGame());
+            _gamesHandlerStub.Games.Add(new MyChessGame());
+            
+            var identity = new ClaimsIdentity();
+            identity.AddClaim(new Claim("http://schemas.microsoft.com/identity/claims/scope", "Games.ReadWrite"));
+            _securityValidatorStub.ClaimsPrincipal = new ClaimsPrincipal(identity);
+
+            var req = HttpRequestHelper.Create("GET");
+
+            // Act
+            var actual = await _gamesFunction.Run(req, null);
+
+            // Assert
+            Assert.IsType(expected, actual);
+            var body = actual as OkObjectResult;
+            var list = body.Value as List<MyChessGame>;
+            Assert.Equal(expectedGames, list.Count);
+        }
+
+        [Fact]
+        public async Task Fetch_Single_Game_Test()
+        {
+            // Arrange
+            var expected = typeof(OkObjectResult);
+            var expectedGameID = "abc";
+
+            _gamesHandlerStub.SingleGame = new MyChessGame()
+            {
+                ID = "abc"
+            };
+
+            var identity = new ClaimsIdentity();
+            identity.AddClaim(new Claim("http://schemas.microsoft.com/identity/claims/scope", "Games.ReadWrite"));
+            _securityValidatorStub.ClaimsPrincipal = new ClaimsPrincipal(identity);
+
+            var req = HttpRequestHelper.Create("GET");
+
+            // Act
+            var actual = await _gamesFunction.Run(req, "abc");
+
+            // Assert
+            Assert.IsType(expected, actual);
+            var body = actual as OkObjectResult;
+            var actualGame = body.Value as MyChessGame;
+            Assert.Equal(expectedGameID, actualGame.ID);
         }
     }
 }
---
+++ test/unit/MyChess.Functions.Tests/Helpers/HttpRequestHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using Moq;
+
+namespace MyChess.Functions.Tests.Helpers
+{
+    public static class HttpRequestHelper
+    {
+        public static HttpRequest Create(string method, Dictionary<string, StringValues> query = null, string body = null)
+        {
+            var reqMock = new Mock<HttpRequest>();
+            reqMock.Setup(req => req.Method).Returns(method);
+
+            if (query != null)
+            {
+                reqMock.Setup(req => req.Query).Returns(new QueryCollection(query));
+            }
+
+            if (body != null)
+            {
+                using var stream = new MemoryStream();
+                using var writer = new StreamWriter(stream);
+                writer.Write(body);
+                writer.Flush();
+                stream.Position = 0;
+                reqMock.Setup(req => req.Body).Returns(stream);
+            }
+
+            return reqMock.Object;
+        }
+    }
+}
