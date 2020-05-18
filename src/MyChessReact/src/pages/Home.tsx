b189b0b092095ab4c2f88efda323ec5661d3123b
Janne Mattila <janne.mattila@hotmail.fi>
2020-05-18 20:39:48 +0300

    Change home page to be functional component

--- src/MyChessReact/src/pages/Home.tsx
+++ src/MyChessReact/src/pages/Home.tsx
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { GameList } from "../components/GameList";
 import "./Home.css";
 import { GetConfiguration } from "../ConfigurationManager";
 
 let configuration = GetConfiguration();
 
-export class Home extends Component {
-    static displayName = Home.name;
-
-    render() {
-        return (
-            <div>
-                <header className="Home-header">
-                    <GameList title="Games waiting for you" endpoint={configuration.endpoint} />
-                </header>
-            </div>
-        );
-    }
+export function Home() {
+    return (
+        <div>
+            <header className="Home-header">
+                <GameList title="Games waiting for you" endpoint={configuration.endpoint} />
+            </header>
+        </div>
+    );
 }
