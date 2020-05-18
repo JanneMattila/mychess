33f4757010a2d7e3519d64d9d159259b4d020906
Janne Mattila <janne.mattila@hotmail.fi>
2020-05-18 14:21:37 +0300

    Add friends function tests

---
+++ test/unit/MyChess.Functions.Tests/FriendsFunctionTests.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+using MyChess.Functions.Tests.Helpers;
+using MyChess.Functions.Tests.Stubs;
+using MyChess.Interfaces;
+using Xunit;
+
+namespace MyChess.Functions.Tests
+{
+    public class FriendsFunctionTests
+    {
+        private readonly FriendsFunction _friendsFunction;
+        private readonly SecurityValidatorStub _securityValidatorStub;
+        private readonly FriendsHandlerStub _friendsHandlerStub;
+
+        public FriendsFunctionTests()
+        {
+            _friendsHandlerStub = new FriendsHandlerStub();
+            _securityValidatorStub = new SecurityValidatorStub();
+            _friendsFunction = new FriendsFunction(NullLogger<FriendsFunction>.Instance, _friendsHandlerStub, _securityValidatorStub);
+        }
+
+        [Fact]
+        public async Task No_ClaimsPrincipal_Test()
+        {
+            // Arrange
+            var expected = typeof(UnauthorizedResult);
+
+            // Act
+            var actual = await _friendsFunction.Run(null, null);
+
+            // Assert
+            Assert.IsType(expected, actual);
+        }
+
+        [Fact]
+        public async Task No_Required_Permission_Test()
+        {
+            // Arrange
+            var expected = typeof(UnauthorizedResult);
+            _securityValidatorStub.ClaimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity());
+
+            // Act
+            var actual = await _friendsFunction.Run(null, null);
+
+            // Assert
+            Assert.IsType(expected, actual);
+        }
+
+        [Fact]
+        public async Task Fetch_Players_Test()
+        {
+            // Arrange
+            var expected = typeof(OkObjectResult);
+            var expectedFriends = 2;
+            
+            _friendsHandlerStub.Friends.Add(new Player());
+            _friendsHandlerStub.Friends.Add(new Player());
+            
+            var identity = new ClaimsIdentity();
+            identity.AddClaim(new Claim("http://schemas.microsoft.com/identity/claims/scope", "User.ReadWrite"));
+            _securityValidatorStub.ClaimsPrincipal = new ClaimsPrincipal(identity);
+
+            var req = HttpRequestHelper.Create("GET");
+
+            // Act
+            var actual = await _friendsFunction.Run(req, null);
+
+            // Assert
+            Assert.IsType(expected, actual);
+            var body = actual as OkObjectResult;
+            var list = body?.Value as List<Player>;
+            Assert.Equal(expectedFriends, list?.Count);
+        }
+
+        [Fact]
+        public async Task Fetch_Single_friend_Test()
+        {
+            // Arrange
+            var expected = typeof(OkObjectResult);
+            var expectedPlayerID = "abc";
+
+            _friendsHandlerStub.SingleFriend = new Player()
+            {
+                ID = "abc"
+            };
+
+            var identity = new ClaimsIdentity();
+            identity.AddClaim(new Claim("http://schemas.microsoft.com/identity/claims/scope", "User.ReadWrite"));
+            _securityValidatorStub.ClaimsPrincipal = new ClaimsPrincipal(identity);
+
+            var req = HttpRequestHelper.Create("GET");
+
+            // Act
+            var actual = await _friendsFunction.Run(req, "abc");
+
+            // Assert
+            Assert.IsType(expected, actual);
+            var body = actual as OkObjectResult;
+            var actualPlayer = body?.Value as Player;
+            Assert.Equal(expectedPlayerID, actualPlayer?.ID);
+        }
+
+        [Fact]
+        public async Task Add_Friend_Test()
+        {
+            // Arrange
+            var expected = typeof(CreatedResult);
+            var expectedPlayerID = "abc";
+            var friend = new Player()
+            {
+                ID = "abc",
+                Name = "great game"
+            };
+
+            _friendsHandlerStub.SingleFriend = new Player()
+            {
+                ID = "abc"
+            };
+
+            var identity = new ClaimsIdentity();
+            identity.AddClaim(new Claim("http://schemas.microsoft.com/identity/claims/scope", "User.ReadWrite"));
+            _securityValidatorStub.ClaimsPrincipal = new ClaimsPrincipal(identity);
+
+            var req = HttpRequestHelper.Create("POST", body: friend);
+
+            // Act
+            var actual = await _friendsFunction.Run(req, "abc");
+
+            // Assert
+            Assert.IsType(expected, actual);
+            var body = actual as CreatedResult;
+            var actualPlayer = body?.Value as Player;
+            Assert.Equal(expectedPlayerID, actualPlayer?.ID);
+        }
+    }
+}
---
+++ test/unit/MyChess.Functions.Tests/Stubs/FriendsHandlerStub.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MyChess.Handlers;
+using MyChess.Interfaces;
+using MyChess.Models;
+
+namespace MyChess.Functions.Tests.Stubs
+{
+    public class FriendsHandlerStub : IFriendsHandler
+    {
+        public Player SingleFriend { get; set; } = new Player();
+
+        public List<Player> Friends { get; set; } = new List<Player>();
+
+        public async Task<(Player? Friend, HandlerError? Error)> AddNewFriend(AuthenticatedUser authenticatedUser, MyChessGame game)
+        {
+            await Task.CompletedTask;
+            return (SingleFriend, null);
+        }
+
+        public async Task<Player?> GetFriendAsync(AuthenticatedUser authenticatedUser, string friendID)
+        {
+            return await Task.FromResult(SingleFriend);
+        }
+
+        public async Task<List<Player>> GetFriendsAsync(AuthenticatedUser authenticatedUser)
+        {
+            return await Task.FromResult(Friends);
+        }
+    }
+}
