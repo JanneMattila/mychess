899fafee7ceaea6ad9340bed99018a4da4261339
Janne Mattila <janne.mattila@hotmail.fi>
2020-05-21 18:44:24 +0300

    Update privacy page

--- src/MyChessReact/src/pages/Privacy.tsx
+++ src/MyChessReact/src/pages/Privacy.tsx
@@ -1,23 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Home.css";
 
-export class Privacy extends Component {
-    static displayName = Privacy.name;
-
-    render() {
-        return (
-            <div>
-                <header className="Home-header">
-                    <h4>Privacy Policy</h4>
-                    <h6>Last updated: April 30th, 2020</h6>
-                    <p>
-                        My Chess uses Microsoft Account or Azure AD account for authentication.<br /><br />
-                        During the authentication process user's name and <b>*hashed*</b> email is stored into our system.<br /><br />
-                        It's only used for identifying users and nothing else. <br /><br />
-                        This information is not shared with anyone.
-                    </p>
-                </header>
-            </div>
-        );
-    }
+export function Privacy() {
+    return (
+        <div>
+            <header className="Home-header">
+                <h4>Privacy Policy</h4>
+                <h6>Last updated: May 21st, 2020</h6>
+                <p>
+                    My Chess uses Microsoft Account or Azure AD account for authentication.<br /><br />
+                    During the authentication process user's name and account identifier is stored into our system.<br /><br />
+                    It's only used for identifying users and nothing else. <br /><br />
+                    This information is not shared with anyone.
+                </p>
+            </header>
+        </div>
+    );
 }
