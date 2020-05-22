037965737205819e786c6970829138b10b302ebe
Janne Mattila <janne.mattila@hotmail.fi>
2020-05-22 17:31:10 +0300

    Add me function tests

---
+++ test/unit/MyChess.Functions.Tests/MeFunctionTests.cs
@@ -0,0 +1,76 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+using MyChess.Functions.Tests.Helpers;
+using MyChess.Functions.Tests.Stubs;
+using Xunit;
+
+namespace MyChess.Functions.Tests
+{
+    public class MeFunctionTests
+    {
+        private readonly MeFunction _meFunction;
+        private readonly SecurityValidatorStub _securityValidatorStub;
+        private readonly MeHandlerStub _meHandlerStub;
+
+        public MeFunctionTests()
+        {
+            _meHandlerStub = new MeHandlerStub();
+            _securityValidatorStub = new SecurityValidatorStub();
+            _meFunction = new MeFunction(NullLogger<MeFunction>.Instance, _meHandlerStub, _securityValidatorStub);
+        }
+
+        [Fact]
+        public async Task No_ClaimsPrincipal_Test()
+        {
+            // Arrange
+            var expected = typeof(UnauthorizedResult);
+
+            // Act
+            var actual = await _meFunction.Run(null);
+
+            // Assert
+            Assert.IsType(expected, actual);
+        }
+
+        [Fact]
+        public async Task No_Required_Permission_Test()
+        {
+            // Arrange
+            var expected = typeof(UnauthorizedResult);
+            _securityValidatorStub.ClaimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity());
+
+            // Act
+            var actual = await _meFunction.Run(null);
+
+            // Assert
+            Assert.IsType(expected, actual);
+        }
+
+        [Fact]
+        public async Task Fetch_Me_Test()
+        {
+            // Arrange
+            var expected = typeof(OkObjectResult);
+            var expectedUserID = "abc";
+
+            _meHandlerStub.UserID = "abc";
+            
+            var identity = new ClaimsIdentity();
+            identity.AddClaim(new Claim("http://schemas.microsoft.com/identity/claims/scope", "User.ReadWrite"));
+            _securityValidatorStub.ClaimsPrincipal = new ClaimsPrincipal(identity);
+
+            var req = HttpRequestHelper.Create("GET");
+
+            // Act
+            var actual = await _meFunction.Run(req);
+
+            // Assert
+            Assert.IsType(expected, actual);
+            var body = actual as OkObjectResult;
+            var actualUserID = body?.Value as string;
+            Assert.Equal(expectedUserID, actualUserID);
+        }
+    }
+}
---
+++ test/unit/MyChess.Functions.Tests/Stubs/MeHandlerStub.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using MyChess.Handlers;
+using MyChess.Interfaces;
+
+namespace MyChess.Functions.Tests.Stubs
+{
+    public class MeHandlerStub : IMeHandler
+    {
+        public string UserID { get; set; } = string.Empty;
+
+        public async Task<string> LoginAsync(AuthenticatedUser authenticatedUser)
+        {
+            return await Task.FromResult(UserID);
+        }
+    }
+}
