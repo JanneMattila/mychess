c704b44e97663714dfed4ed266ad904e9c5159d2
Janne Mattila <janne.mattila@hotmail.fi>
2020-06-04 21:42:29 +0300

    Load player identifier from database

--- src/MyChessReact/src/pages/SettingsPage.tsx
+++ src/MyChessReact/src/pages/SettingsPage.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, MouseEvent, useState } from "react";
 import { Link } from "react-router-dom";
 import Switch from "react-switch";
 import "./SettingsPage.css";
-import { RootState, ProcessState, meLoadingEvent } from "../actions";
+import { ProcessState, meLoadingEvent } from "../actions";
 import { useTypedSelector } from "../reducers";
 import { getAppInsights } from "../components/TelemetryService";
 import { Player } from "../models/Player";
@@ -14,11 +14,9 @@ type SettingsProps = {
 };
 
 export function SettingsPage(props: SettingsProps) {
-    const selectorLoginState = (state: RootState) => state.loginState;
-    const selectorAccessToken = (state: RootState) => state.accessToken;
-
-    const loginState = useTypedSelector(selectorLoginState);
-    const accessToken = useTypedSelector(selectorAccessToken);
+    const loginState = useTypedSelector(state => state.loginState);
+    const accessToken = useTypedSelector(state => state.accessToken);
+    const me = Database.get<string>(DatabaseFields.ME_ID);
 
     const [playerIdentifier, setPlayerIdentifier] = useState("");
     const [isNotificationsEnabled, setNotifications] = useState(false);
@@ -57,9 +55,12 @@ export function SettingsPage(props: SettingsProps) {
         }
 
         if (accessToken !== undefined) {
+            if (me) {
+                setPlayerIdentifier(me);
+            }
             populateUserInformation(accessToken);
         }
-    }, [accessToken, ai, props.endpoint, dispatch]);
+    }, [accessToken, ai, props.endpoint, dispatch, me]);
 
     const confirm = (event: MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
