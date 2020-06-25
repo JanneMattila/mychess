b8d8c33faa0fd080c304c727a0c9631543dc7e0a
Janne Mattila <janne.mattila@hotmail.fi>
2020-06-25 22:09:50 +0300

    Fix unit test stub

--- test/unit/MyChess.Functions.Tests/Stubs/SecurityValidatorStub.cs
+++ test/unit/MyChess.Functions.Tests/Stubs/SecurityValidatorStub.cs
@@ -6,7 +6,13 @@ namespace MyChess.Functions.Tests.Stubs
 {
     public class SecurityValidatorStub : ISecurityValidator
     {
+        public bool InitializationResult { get; set; } = true;
         public ClaimsPrincipal? ClaimsPrincipal { get; set; }
+
+        public async Task<bool> InitializeAsync()
+        {
+            return await Task.FromResult(InitializationResult);
+        }
 
         public async Task<ClaimsPrincipal?> GetClaimsPrincipalAsync(HttpRequest req)
         {
