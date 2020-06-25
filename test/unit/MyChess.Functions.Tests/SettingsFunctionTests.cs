b4a473f1f692cf57d65e41873368938e2eb155b5
Janne Mattila <janne.mattila@hotmail.fi>
2020-06-25 16:35:53 +0300

    Add settings function unit tests

---
+++ test/unit/MyChess.Functions.Tests/SettingsFunctionTests.cs
@@ -0,0 +1,125 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+using MyChess.Functions.Tests.Helpers;
+using MyChess.Functions.Tests.Stubs;
+using MyChess.Interfaces;
+using MyChess.Models;
+using Xunit;
+
+namespace MyChess.Functions.Tests
+{
+    public class SettingsFunctionTests
+    {
+        private readonly SettingsFunction _settingsFunction;
+        private readonly SecurityValidatorStub _securityValidatorStub;
+        private readonly SettingsHandlerStub _settingsHandlerStub;
+
+        public SettingsFunctionTests()
+        {
+            _settingsHandlerStub = new SettingsHandlerStub();
+            _securityValidatorStub = new SecurityValidatorStub();
+            _settingsFunction = new SettingsFunction(NullLogger<SettingsFunction>.Instance, _settingsHandlerStub, _securityValidatorStub);
+        }
+
+        [Fact]
+        public async Task No_ClaimsPrincipal_Test()
+        {
+            // Arrange
+            var expected = typeof(UnauthorizedResult);
+
+            // Act
+            var actual = await _settingsFunction.Run(null);
+
+            // Assert
+            Assert.IsType(expected, actual);
+        }
+
+        [Fact]
+        public async Task No_Required_Permission_Test()
+        {
+            // Arrange
+            var expected = typeof(UnauthorizedResult);
+            _securityValidatorStub.ClaimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity());
+
+            // Act
+            var actual = await _settingsFunction.Run(null);
+
+            // Assert
+            Assert.IsType(expected, actual);
+        }
+
+        [Fact]
+        public async Task Fetch_Settings_Test()
+        {
+            // Arrange
+            var expected = typeof(OkObjectResult);
+            var expectedPlayAlwaysUp = true;
+
+            _settingsHandlerStub.UserSettings.PlayAlwaysUp = true;
+            
+            var identity = new ClaimsIdentity();
+            identity.AddClaim(new Claim("http://schemas.microsoft.com/identity/claims/scope", "User.ReadWrite"));
+            _securityValidatorStub.ClaimsPrincipal = new ClaimsPrincipal(identity);
+
+            var req = HttpRequestHelper.Create("GET");
+
+            // Act
+            var actual = await _settingsFunction.Run(req);
+
+            // Assert
+            Assert.IsType(expected, actual);
+            var body = actual as OkObjectResult;
+            var actualUserSettings = body?.Value as UserSettings;
+            Assert.Equal(expectedPlayAlwaysUp, actualUserSettings?.PlayAlwaysUp);
+        }
+
+        [Fact]
+        public async Task Update_Settings_Test()
+        {
+            // Arrange
+            var expected = typeof(OkResult);
+
+            var userSettings = new UserSettings();
+            
+            var identity = new ClaimsIdentity();
+            identity.AddClaim(new Claim("http://schemas.microsoft.com/identity/claims/scope", "User.ReadWrite"));
+            _securityValidatorStub.ClaimsPrincipal = new ClaimsPrincipal(identity);
+
+            var req = HttpRequestHelper.Create("POST", body: userSettings);
+
+            // Act
+            var actual = await _settingsFunction.Run(req);
+
+            // Assert
+            Assert.IsType(expected, actual);
+        }
+
+
+        [Fact]
+        public async Task Update_Settings_With_Failure_Test()
+        {
+            // Arrange
+            var expected = typeof(ObjectResult);
+
+            var userSettings = new UserSettings();
+            _settingsHandlerStub.Error = new HandlerError()
+            {
+                Status = 501
+            };
+
+            var identity = new ClaimsIdentity();
+            identity.AddClaim(new Claim("http://schemas.microsoft.com/identity/claims/scope", "User.ReadWrite"));
+            _securityValidatorStub.ClaimsPrincipal = new ClaimsPrincipal(identity);
+
+            var req = HttpRequestHelper.Create("POST", body: userSettings);
+
+            // Act
+            var actual = await _settingsFunction.Run(req);
+
+            // Assert
+            Assert.IsType(expected, actual);
+        }
+    }
+}
---
+++ test/unit/MyChess.Functions.Tests/Stubs/SettingsHandlerStub.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using MyChess.Handlers;
+using MyChess.Interfaces;
+using MyChess.Models;
+
+namespace MyChess.Functions.Tests.Stubs
+{
+    public class SettingsHandlerStub : ISettingsHandler
+    {
+        public UserSettings UserSettings { get; set; } = new UserSettings();
+
+        public HandlerError? Error { get; set; }
+
+        public async Task<UserSettings> GetSettingsAsync(AuthenticatedUser authenticatedUser)
+        {
+            return await Task.FromResult(UserSettings);
+        }
+
+        public async Task<HandlerError?> UpdateSettingsAsync(AuthenticatedUser authenticatedUser, UserSettings playerSettings)
+        {
+            return await Task.FromResult(Error);
+        }
+    }
+}
