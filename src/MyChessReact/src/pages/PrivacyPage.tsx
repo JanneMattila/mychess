755d7bad21dc7e09b323f867417d15db4e5ba0c2
Janne Mattila <janne.mattila@hotmail.fi>
2020-08-14 12:34:45 +0300

    Update privacy page due to MSAL changes

--- src/MyChessReact/src/pages/PrivacyPage.tsx
+++ src/MyChessReact/src/pages/PrivacyPage.tsx
@@ -5,11 +5,11 @@ export function PrivacyPage() {
     return (
         <div>
             <div className="title">Privacy Policy</div>
-            <div className="lastUpdatedText">Last updated: May 21st, 2020</div>
+            <div className="lastUpdatedText">Last updated: Aug 14th, 2020</div>
             <div className="privacyText-Container">
                 <div className="privacyText">
                     My Chess uses Microsoft Account or Azure AD account for authentication.<br /><br />
-                    During the authentication process user's name and account identifier is stored into our system.<br /><br />
+                    During the authentication process user's account email address and technical identifier are stored into our system.<br /><br />
                     It's only used for identifying users and nothing else. <br /><br />
                     This information is not shared with anyone.
                 </div>
