5497f46a68add0aa612e9e481d0d8f506934978f
Janne Mattila <janne.mattila@hotmail.fi>
2021-10-12 19:37:36 +0300

    Add cookie consent support

---
+++ src/MyChess.Client/wwwroot/js/app.js
@@ -0,0 +1,9 @@
+﻿var MyChess = MyChess || {};
+
+MyChess.isCookieConsentAccepted = () => {
+    return document.cookie.indexOf("MyChessCookieConsent=true") >= 0;
+}
+
+MyChess.acceptCookieConsent = () => {
+    document.cookie = `MyChessCookieConsent=true; secure; samesite=strict`;
+}
