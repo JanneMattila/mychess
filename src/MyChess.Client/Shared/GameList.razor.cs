34b38d79c7be9003279b262d13d3c35c915bf444
Janne Mattila <janne.mattila@hotmail.fi>
2021-10-22 16:34:35 +0300

    Add game list filters

--- src/MyChess.Client/Shared/GameList.razor.cs
+++ src/MyChess.Client/Shared/GameList.razor.cs
@@ -6,6 +6,9 @@ namespace MyChess.Client.Shared;
 [Authorize]
 public class GameListBase : MyChessComponentBase
 {
+    protected bool ShowFilters { get; set; } = false;
+    protected string Filters { get; set; } = GameFilterType.WaitingForYou;
+    protected string Title { get; set; } = "Games waiting for you";
     protected List<MyChessGame> Games { get; set; }
 
     protected override async Task OnInitializedAsync()
@@ -16,12 +19,39 @@ public class GameListBase : MyChessComponentBase
     protected async Task RefreshGames()
     {
         AppState.IsLoading = true;
-        Games = await Client.GetGamesAsync();
+        ShowFilters = false;
+        Games = await Client.GetGamesAsync(Filters);
         AppState.IsLoading = false;
     }
 
     protected void AddNewGame()
     {
         NavigationManager.NavigateTo("/friends");
+    }
+
+    protected void ToggleFilterVisibility()
+    {
+        ShowFilters = !ShowFilters;
+    }
+
+    protected async Task WaitingForYou()
+    {
+        Title = "Games waiting for you";
+        Filters = GameFilterType.WaitingForYou;
+        await RefreshGames();
+    }
+
+    protected async Task WaitingForOpponent()
+    {
+        Title = "Games waiting for opponent";
+        Filters = GameFilterType.WaitingForOpponent;
+        await RefreshGames();
+    }
+
+    protected async Task Archive()
+    {
+        Title = "Archive";
+        Filters = GameFilterType.Archive;
+        await RefreshGames();
     }
 }
